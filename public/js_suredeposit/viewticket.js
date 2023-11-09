bb2e53e6bb27702d67cdf04607c3fcfd6be9b52c
Anirudh Bagalkotker <93025106+AnirudhBagalkotker@users.noreply.github.com>
2023-11-09 16:36:10 +0530

    Bug Fix: Date issue in View Tickets

--- public/js_suredeposit/viewticket.js
+++ public/js_suredeposit/viewticket.js
@@ -1,35 +1,28 @@
 const tickets = document.getElementById("tickets");
 const box = document.getElementById("ticketBox");
 
-fetch('/getData/help')
-	.then(response => response.json())
-	.then(data => {
-		const { tick, result } = data;
-		tickets.innerHTML = tick;
-		result.forEach(ticket => {
-			var solved = ticket.SOLVED;
-			var color = 'yellow-box';
-			if (solved == 1) {
-				solved = "Solved";
-				color = 'green-box';
-			}
-			else {
-				solved = "Pending";
-			}
-			var date = ticket.TICKDATE;
-			const [tdate, extra] = date.split('T');
+fetch('/getData/help').then(response => response.json()).then(data => {
+	const { tick, result } = data;
+	tickets.innerHTML = tick;
+	result.forEach(ticket => {
+		let solved = ticket.SOLVED;
+		let color = 'yellow-box';
+		if (solved === 1) {
+			solved = "Solved";
+			color = 'green-box';
+		}
+		else solved = "Pending";
+		let date = new Date(ticket.TICKDATE);
 
-			box.innerHTML += ` 
+		box.innerHTML += ` 
 				<div class="colored-box ${color} flex-colunm ">
 					<span class="colored-item h-6 b"> ${ticket.TITLE} </span>
 					<span class="colored-item text2"> ${ticket.ISSUE} </span>
 					<div class="colored-item flex-row">
-						<span class="text2 b"> ${tdate} </span>
+						<span class="text2 b"> ${date.toString().substring(4, 15)} </span>
 						<span class="text2 b"> ${solved} </span>
 					</div>
 				</div>
 			`
-		});
-
-	})
-	.catch(error => console.error(error));
+	});
+}).catch(error => console.error(error));
